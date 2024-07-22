84a33c41e0479b4b03e10cfd9910e8b3058208bb
invernoe <mahmoudk221999@gmail.com>
2024-07-22 22:22:31 +0300

    finish project 1

--- Project1/Point3D.cs
+++ Project1/Point3D.cs
@@ -6,8 +6,37 @@ using System.Threading.Tasks;
 
 namespace Project1
 {
-    internal class Point3D
+    internal class Point3D : IComparable, ICloneable
     {
+        public int X {  get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
 
+        public Point3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Point3D() : this(0, 0, 0)
+        { 
+        }
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Point3D passedPoint = (Point3D) obj;
+            return this.X - passedPoint.X == 0 ? (this.Y - passedPoint.Y) : this.X - passedPoint.X;
+        }
+
+        public object Clone()
+        {
+            return new Point3D(X, Y, Z);
+        }
     }
 }
--- Project1/Program.cs
+++ Project1/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 2
+            Point3D P = new Point3D(10, 10, 10);
+            Console.WriteLine(P.ToString());
+            #endregion
+
+            #region Part 3
+            Point3D[] points = new Point3D[2];
+            for (int i = 1; i < 3; i++)
+            {
+                bool isAllParsed = true;
+
+                Console.WriteLine($"enter X for point {i}: ");
+                isAllParsed &= int.TryParse(Console.ReadLine(), out int x);
+                Console.WriteLine($"enter Y for point {i}: ");
+                isAllParsed &= int.TryParse(Console.ReadLine(), out int y);
+                Console.WriteLine($"enter Z for point {i}: ");
+                isAllParsed &= int.TryParse(Console.ReadLine(), out int z);
+
+                if (isAllParsed)
+                    points[i - 1] = new Point3D(x, y, z);
+                else
+                    i--;
+            }
+
+            //does not work because == base implementation compares references
+            Console.WriteLine($"point 1 == point 2?\n{points[0] == points[1]}");
+
+            //array of points
+            Point3D[] points2 =
+            {
+                new Point3D (10, 20, 10),
+                new Point3D (10, 10, 10),
+                new Point3D (30, 30, 30),
+            };
+
+            Array.Sort(points2);
+            foreach (Point3D point in points2)
+            {
+                Console.WriteLine(point);
+            }
+
+            Point3D p1 = new Point3D(10, 20, 30);
+            Point3D p2 = (Point3D) p1.Clone();
+            #endregion
         }
     }
 }
