ae17ef3b4484390b025921af4396e0daa3305866
invernoe <mahmoudk221999@gmail.com>
2024-07-22 22:22:49 +0300

    finish project 2

---
+++ Project2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Maths.Add(5, 4));
+            Console.WriteLine(Maths.Subtract(5, 4));
+            Console.WriteLine(Maths.Divide(5, 4));
+            Console.WriteLine(Maths.Multiply(5, 4));
+        }
+    }
+}
